3c72b506e9a07344528e7e0b0d64b2f369ac3c03
Rafael JPD <rafael.pezzuto@gmail.com>
2025-01-31 15:29:18 -0300

    Cria configurações de menu do wagtail

--- config/menu.py
+++ config/menu.py
@@ -1,13 +1,17 @@
 WAGTAIL_MENU_APPS_ORDER = {
     "collection": 100,
-    "metrics": 200,
-    "log_manager": 300,
-    "tasks": 400,
-    "unexpected-error": 500,
+    "article": 200,
+    "journal": 300,
+    "resources": 400,
+    "log_manager": 500,
+    "log_manager_config": 600,
+    "metrics": 700,
+    "tasks": 800,
+    "unexpected-error": 900,
 }
 
 def get_menu_order(app_name):
     try:
         return WAGTAIL_MENU_APPS_ORDER[app_name]
     except:
-        return 900
+        return 950
