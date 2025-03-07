4837dbf363c9c71cadeffe26458e32319b020c04
Rafael JPD <rafael.pezzuto@gmail.com>
2025-03-07 13:54:00 -0300

    Atualiza wagtailhooks conforme modelos de metrics

--- metrics/wagtail_hooks.py
+++ metrics/wagtail_hooks.py
@@ -3,64 +3,100 @@ from wagtail.snippets.views.snippets import SnippetViewSet, SnippetViewSetGroup
 from wagtail.snippets.models import register_snippet
 
 from config.menu import get_menu_order
-from .models import Top100Articles, Top100ArticlesFile
+from .models import Item, ItemAccess, UserSession, UserAgent
 
 
-class Top100ArticlesSnippetViewSet(SnippetViewSet):
-    model = Top100Articles
-    menu_label = _("Top 100 Articles")
+class ItemSnippetViewSet(SnippetViewSet):
+    model = Item
+    menu_label = _("Item")
     icon = "list-ol"
     menu_order = 100
 
     list_display = (
         'collection',
-        'pid_issn',
-        'pid',
-        'yop',
-        'year_month_day',
-        'total_item_requests',
-        'total_item_investigations',
-        'unique_item_requests',
-        'unique_item_investigations',
+        'journal',
+        'article',
     )
     list_filter = (
         "collection",
-        "pid_issn",
-        'yop',
-        'year_month_day',
+        "journal",
     )
     search_fields = (
-        "pid",
+        "journal",
+        "article",
     )
 
 
-class Top100ArticlesFileSnippetViewSet(SnippetViewSet):
-    model = Top100ArticlesFile
-    menu_label = _("Top 100 Articles File")
+class ItemAccessSnippetViewSet(SnippetViewSet):
+    model = ItemAccess
+    menu_label = _("Item Access")
     icon = "list-ol"
-    menu_order = 100
+    menu_order = 200
 
     list_display = (
-        'attachment',
-        'get_status_display',
-        'creator',
-        'created',
-        'updated',
+        'item',
+        'user_session',
+        'country_code',
+        'media_language',
+        'media_format',
     )
     list_filter = (
-        "status",
+        "item",
+        "country_code",
+        "media_language",
+        "media_format",
+    )
+    search_fields = (
+        "item",
+    )
+
+
+class UserSessionSnippetViewSet(SnippetViewSet):
+    model = UserSession
+    menu_label = _("User Session")
+    icon = "list-ol"
+    menu_order = 300
+
+    list_display = (
+        'datetime',
+        'user_agent',
+        'user_ip',
+    )
+    list_filter = (
+        "datetime",
+    )
+    search_fields = (
+        "user_agent",
+        "user_ip",
+    )
+
+
+class UserAgentSnippetViewSet(SnippetViewSet):
+    model = UserAgent
+    menu_label = _("User Agent")
+    icon = "list-ol"
+    menu_order = 400
+
+    list_display = (
+        'name',
+        'version',
+    )
+    search_fields = (
+        "name",
     )
 
 
 class MetricsViewSetGroup(SnippetViewSetGroup):
     menu_name = 'metrics'
-    menu_label = _("Usage Metrics")
+    menu_label = _("Metrics")
     icon = "folder-open-inverse"
     menu_order = get_menu_order("metrics")
     
     items = (
-        Top100ArticlesSnippetViewSet, 
-        Top100ArticlesFileSnippetViewSet,
+        ItemAccessSnippetViewSet,
+        ItemSnippetViewSet,
+        UserSessionSnippetViewSet,
+        UserAgentSnippetViewSet,
     )
 
 
