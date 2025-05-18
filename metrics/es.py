7cdfc6add19e9bd8ab5cbe3d51a33e168b56584b
Rafael JPD <rafael.pezzuto@gmail.com>
2025-05-18 13:10:52 -0300

    Cria método para criar índice ES

--- metrics/es.py
+++ metrics/es.py
@@ -29,3 +29,76 @@ def get_elasticsearch_client(url=None, basic_auth=None, api_key=None):
         client = Elasticsearch(url)
 
     return client
+
+
+def create_index(index_name, mappings=None, client=None, url=None, basic_auth=None, api_key=None):
+    """
+    Create an Elasticsearch index. 
+
+    :param index_name: Name of the index to create.
+    :param mappings: Mappings for the index. If None, default mappings will be used.
+    :param client: Elasticsearch client instance. If None, a new client will be created.
+    :param url: Elasticsearch URL. If None, it will be taken from Django settings.
+    :param basic_auth: Basic authentication credentials. If None, it will be taken from Django settings.
+    :param api_key: API key. If None, it will be taken from Django settings.
+    """
+    if not client:
+        client = get_elasticsearch_client(url, basic_auth, api_key)
+
+    if not mappings:
+        mappings = {
+            "properties": {
+                "collection": {
+                    "type": "keyword"
+                },
+                "journal": {
+                    "type": "keyword"
+                },
+                "pid_v2": {
+                    "type": "keyword"
+                },
+                "pid_v3": {
+                    "type": "keyword"
+                },
+                "pid_generic": {
+                    "type": "keyword"
+                },
+                "media_language": {
+                    "type": "keyword"
+                },
+                "country_code": {
+                    "type": "keyword"
+                },
+                "date": {
+                    "type": "date",
+                    "format": "yyyy-MM-dd"
+                },
+                "year": {
+                    "type": "integer"
+                },
+                "month": {
+                    "type": "integer"
+                },
+                "day": {
+                    "type": "integer"
+                },
+                "total_requests": {
+                    "type": "integer"
+                },
+                "total_investigations": {
+                    "type": "integer"
+                },
+                "unique_requests": {
+                    "type": "integer"
+                },
+                "unique_investigations": {
+                    "type": "integer"
+                }
+            }
+        }
+
+    resp = client.indices.create(
+        index=index_name,
+        mappings=mappings,
+    )
+    logging.info(f"Index {index_name} created: {resp}")
