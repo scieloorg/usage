f0865d7ac764127e98bdaeee1f7d895c36d0e4f6
Rafael JPD <rafael.pezzuto@gmail.com>
2025-05-18 13:10:26 -0300

    Cria método para obter cliente ES

--- metrics/es.py
+++ metrics/es.py
@@ -4,3 +4,28 @@ from django.conf import settings
 import logging
 
 
+def get_elasticsearch_client(url=None, basic_auth=None, api_key=None):
+    """
+    Create an Elasticsearch client instance using Django settings.
+
+    :param url: Elasticsearch URL. If None, it will be taken from Django settings.
+    :param basic_auth: Basic authentication credentials. If None, it will be taken from Django settings.
+    :param api_key: API key. If None, it will be taken from Django settings.
+    """
+    if not url:
+        url = getattr(settings, "ES_URL", None)
+
+    if not basic_auth:
+        basic_auth = getattr(settings, "ES_BASIC_AUTH", None)
+
+    if not api_key:
+        api_key = getattr(settings, "ES_API_KEY", None)
+
+    if basic_auth:
+        client = Elasticsearch(url, basic_auth=basic_auth)
+    elif api_key:
+        client = Elasticsearch(url, api_key=api_key)
+    else:
+        client = Elasticsearch(url)
+
+    return client
