7c7304fe7bf0546bd792960bd50151307a3a064f
Rafael Damaceno <rafael@pitangainnovare.com.br>
2026-09-10 22:19:07 -0300

    Registra alterações de metadados na outbox

--- metrics/apps.py
+++ metrics/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class MetricsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "metrics"
+
+    def ready(self):
+        from metrics import signals  # noqa: F401
